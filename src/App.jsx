0bd010959a54a209c9ceaec7a2b3ebc87cb9fd36
tatsumaru20516 <rixen71@gmail.com>
2025-07-17 11:48:17 +0900

    feat:詳細なポケモンデータの取得を実装

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ function App() {
   // ポケモンデータを格納するステート
   const [pokemonData, setPokemonData] = useState([]);
 
-  // 全ポケモンデータを格納する配列
-  const [allPokemonData, setAllPokemonData] = useState([]);
-
   // 初期URLを設定
   const initialUrl = `https://pokeapi.co/api/v2/pokemon`;
 
@@ -17,13 +14,29 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchPokemonData(initialUrl); //dataはJSON
-      console.log(data); // デバッグ用
+      // console.log(data); // デバッグ用
+      loadPokemon(data.results); // 詳細なポケモンデータの配列をpokemonDataに保存
     };
     fetchData();
   }, []);
 
-  // resultsから1匹分のポケモンデータを取得するloadPokemon関数
+  // // デバッグ
+  // useEffect(() => {
+  //   console.log(pokemonData); // pokemonDataの内容を確認
+  // }, [pokemonData]);
 
+  // 引数：results配列[{name,url},{〃}...]　戻り値：全ポケモン配列[1匹の詳細, 2匹目の詳細, ...]
+  const loadPokemon = async (allPokemonData) => {
+    const pokemonDataArray = await Promise.all(
+      allPokemonData.map( pokemon => {
+        let pokemonJson = fetchPokemonData(pokemon.url);
+        return pokemonJson;
+      })
+    );
+    setPokemonData(pokemonDataArray); // ポケモンデータをステートに保存
+  };
+    // ここでポケモンデータを処理するロジックを追加できます
+    // console.log(allPokemonData); // デバッグ用
 
   return (
     <>
